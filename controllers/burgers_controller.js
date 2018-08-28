0c52503b3cb9cdcff2c5edd6d0c7dafb1e08438e
griffinhall2 <griffin.hall@yahoo.com>
2018-08-27 22:17:46 -0400

    update controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,10 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
+router.get('/', function(req,res) {
+	res.redirect('/burgers')
+});
+
 router.get("/burgers", function (req, res) {
     burger.all(function (data) {
         var hbsObject = {
