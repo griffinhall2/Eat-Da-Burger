8e619fb72647c39d31ac92b5c172253b972cecd0
griffinhall2 <griffin.hall@yahoo.com>
2018-08-28 07:42:39 -0400

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
